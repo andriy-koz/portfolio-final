fe0b48ee525248f11d4f9d10175979430811faa6
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-12-22 00:49:45 -0300

    feat: header

---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+
+const Header = () => {
+  return (
+    <header className='h-screen bg-hero-pattern bg-cover bg-top clip-pattern relative'>
+      <div className='absolute top-7 left-7'>
+        <Image src='/logo-white.png' alt='logo' height={35} width={70} />
+      </div>
+      <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-full text-center'>
+        <h1 className='text-white uppercase font-semibold'>
+          <span className='block text-6xl tracking-wider animate-in-left'>
+            KOZ DEV
+          </span>
+          <span className='block text-xl tracking-wide mt-3 animate-in-right'>
+            Desarrollo web y marketing digital
+          </span>
+        </h1>
+        <a
+          href='#'
+          className='uppercase py-4 px-10 bg-white color text-zinc-900 inline-block hover:-translate-y-1 active:-translate-y-0.5 mt-14 rounded-full transition-all hover:shadow-lg active:shadow-md animation-fill-back animate-in-bottom'>
+          Explora mis proyectos
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,33 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern':
+          'linear-gradient(to right bottom, rgba(167, 139, 250, 0.8), rgba(91, 33, 182, 0.8)), url("/hero.webp")',
+      },
+      keyframes: {
+        'in-left': {
+          '0%': { transform: 'translateX(-100px)', opacity: 0 },
+          '80%': { transform: 'translateX(10px)' },
+          '100%': { opacity: 1, transform: 'translate(0)' },
+        },
+        'in-right': {
+          '0%': { transform: 'translateX(100px)', opacity: 0 },
+          '80%': { transform: 'translateX(-10px)' },
+          '100%': { opacity: 1, transform: 'translate(0)' },
+        },
+        'in-bottom': {
+          '0%': { transform: 'translateY(30px)', opacity: 0 },
+          '100%': { opacity: 1, transform: 'translate(0)' },
+        },
+      },
+      animation: {
+        'in-left': 'in-left 1s ease-out',
+        'in-right': 'in-right 1s ease-out',
+        'in-bottom': 'in-bottom .5s ease-out .75s',
+      },
+    },
   },
   plugins: [],
 };

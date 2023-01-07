9dea9198d5e8b2aa05f8cfac335fba5a97a9785d
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-01-07 17:31:58 -0300

    feat: testimonials section

--- components/UI/TestimonialsCard.js
+++ components/UI/TestimonialsCard.js
@@ -1,7 +1,22 @@
-const TestimonialsCard = ({ name, text, title }) => {
+import Image from 'next/image';
+
+const TestimonialsCard = ({ name, text, title, img }) => {
   return (
-    <div className='bg-white h-72 w-[90%] mx-auto -skew-x-12 shadow-lg'>
-      <div className='skew-x-12'></div>
+    <div className='bg-white h-64 w-3/4 mx-auto -skew-x-12 shadow-lg flex justify-center items-center'>
+      <div className='skew-x-12 pr-20 pl-24 mx-auto'>
+        <div className='w-max h-max shape-outside rounded-full overflow-hidden -translate-x-4 mt-2'>
+          <Image
+            height={128}
+            width={128}
+            src={`${img}`}
+            alt='Testimonial portrait'
+          />
+        </div>
+        <div className='text-slate-900 text-left'>
+          <h3 className='text-2xl mb-4'>{title}</h3>
+          <p className='leading-6 text-xl'>{text}</p>
+        </div>
+      </div>
     </div>
   );
 };
--- components/sections/Testimonials.js
+++ components/sections/Testimonials.js
@@ -6,14 +6,16 @@ const Testimonials = () => {
     <section className='flex flex-col justify-center align-middle text-center space-y-10 mt-10'>
       <SubTitle text='Lo que nuestros clientes dicen sobre Koz Dev' />
       <TestimonialsCard
+        img='/testimonial-man.webp'
         name='Carlos Martínez'
         title='Una experiencia de primera clase'
-        text='Desde el primer momento, Koz Dev demostró ser una agencia de desarrollo de software de primer nivel. El equipo es muy experto y siempre estuvo dispuesto a trabajar con nosotros para entender nuestras necesidades y ofrecernos soluciones personalizadas. Además, siempre entregaron a tiempo y fueron un gran apoyo durante todo el proceso. ¡Muy recomendables!'
+        text='Siempre estuvo dispuesto a trabajar con nosotros para entender nuestras necesidades y ofrecernos soluciones personalizadas. Además, siempre entregaron a tiempo y fueron un gran apoyo durante todo el proceso. ¡Muy recomendables!'
       />
       <TestimonialsCard
+        img='/testimonial-woman.webp'
         name='Ana Rodríguez'
         title='Trabajar con Koz Dev ha sido un verdadero placer'
-        text='No podríamos estar más satisfechos con nuestra experiencia trabajando con Koz Dev. El equipo es muy profesional y siempre estuvo dispuesto a ayudarnos a alcanzar nuestras metas tecnológicas. La calidad del trabajo es excelente y el resultado final superó nuestras expectativas. ¡Sin duda los elegiríamos de nuevo para futuros proyectos!'
+        text='El equipo es muy profesional y siempre estuvo dispuesto a ayudarnos a alcanzar nuestras metas tecnológicas. La calidad del trabajo es excelente y el resultado final superó nuestras expectativas. ¡Sin duda los elegiríamos de nuevo para futuros proyectos!'
       />
     </section>
   );
